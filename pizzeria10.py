7843d0749c4942be4d74e2fd63c3986a14566a4d
Daniela Funes <danielafunes1970@gmail.com>
2021-02-02 18:04:36 -0500

    mejora en la construccion de la clase

--- pizzeria10.py
+++ pizzeria10.py
@@ -1,33 +1,20 @@
-# La pizzería Bella Napoli ofrece pizzas vegetarianas y no vegetarianas a sus clientes. Los ingredientes para cada tipo de pizza aparecen a continuación.
-# * Ingredientes vegetarianos: Pimiento y tofu.
-# * Ingredientes no vegetarianos: Peperoni, Jamón y Salmón.
-# Escribir un programa que pregunte al usuario si quiere una pizza vegetariana o no, y en función de su respuesta le muestre un menú con los ingredientes disponibles para que elija. Solo se puede eligir un ingrediente además de la mozzarella y el tomate que están en todas la pizzas. Al final se debe mostrar por pantalla si la pizza elegida es vegetariana o no y todos los ingredientes que lleva. Además también debes preguntale al cliente si quiere un adicional en su elección
-class Pizza():
+class Pizza(): #atributos: tipo y extra
     def __init__(self):
-        self.vegetariana = 'pimentón, tofu, mozzarella y tomate'
-        self.no_vegetariana = 'pepperoni, jamón, salmón, mozzarella y tomate'
-        self.extra = ['Pimentón','Tofu', 'Pepperoni', 'Jamón', 'Salmón', 'mozzarella', 'tomate']
+        self.tipo = ['pimentón, tofu, mozzarella y tomate','pepperoni, jamón, salmón, mozzarella y tomate'] #lista con los dos conjuntos de ingredientes según el tipo de pizza
+        self.extra = ['Pimentón','Tofu', 'Pepperoni', 'Jamón', 'Salmón', 'mozzarella', 'tomate', 'ninguno'] #lista de ingredientes extra
     def __str__(self):
         print('''***BIENVENIDO A LA PIZZERIA BELLA NAPOLI***
             ¿Qué tipo de pizza desea?: 
             Opción 1: Vegetariana 
             Opción 2: No vegetariana''')
         tipo = int(input('Por favor, introduzca el número de opción deseada: '))
+        i = int(input('Escoge uno entre los siguientes extras: pimiento(1), tofu(2), pepperoni(3), jamón(4), salmón(5), mozzarella(6), tomate(7), ninguno(8): '))
         if tipo == 1:
-            return 'Usted escogió una Pizza Vegetariana con {}'.format(self.vegetariana)
+            return'Usted escogió una Pizza Vegetariana con {}y con el ingrediente extra: {} ¡Buen Provecho!'.format(self.tipo[0],self.extra[i-1])
         if tipo == 2:
-            return 'Usted escogió una Pizza no Vegetariana con {}'.format(self.no_vegetariana)
-    def ingrediente_extra(self):
-        ingextra = int(input('¿Desea un extra en su pizza? Si: 1 No: 2')
-        if ingextra == 1
-            i = int(input('Escoge uno entre los siguientes extras: pimiento(1), tofu(2), pepperoni(3), jamón(4), salmón(5), mozzarella(6), tomate(7): '))
-            return 'Usted escogió el ingrediente extra: {} ¡Buen Provecho!'.format(self.extra[i-1])
-        else
-            return '¡Buen Provecho!'
-   
+            return 'Usted escogió una Pizza no Vegetariana con {} y con el ingrediente extra: {} ¡Buen Provecho!'.format(self.tipo[1],self.extra[i-1])
 miPizza = Pizza()
 print(str(miPizza))
-
 
 
 
