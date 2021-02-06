7b57b4c1f9b8d27038ea19451c630d8ad8875544
Daniela Funes <danielafunes1970@gmail.com>
2021-02-06 12:23:51 -0500

    mejora en el output

--- conversorabinario.py
+++ conversorabinario.py
@@ -1,11 +1,12 @@
 def calculo():
     decimal = int(input('Introduce un número: '))
+    elegido = decimal
     binario = ''
     while (decimal > 0):
         resto = str(decimal % 2)
         binario = resto + binario
         decimal = decimal // 2
-    print(str(binario)) 
+    print(str('El número decimal {} es en binario: {}'.format(elegido,binario))) 
            
 if __name__ == "__main__":
     calculo()
